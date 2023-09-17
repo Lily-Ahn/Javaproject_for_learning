6da72c9c1b3989014d81103df1182a0f77bd3c88
Lily <dksgofla0914@naver.com>
2023-09-18 02:01:52 +0900

    조건문 실습 문제 풀이 ( if , else if , switch 문)

---
+++ Java05_Switch/src/com/silsub1/SilsubMain.java
@@ -0,0 +1,19 @@
+package com.silsub1;
+
+public class SilsubMain {
+	
+	public static void main (String[] args) {
+		
+		example_Munjae em = new example_Munjae();
+		
+		//em.test1();
+		//em.test2();
+		//em.test3();
+		//em.test4();
+		//em.inputTest();
+		//em.test6();
+		em.test7();
+		
+	}
+
+}
